10adb3724d5a5486343ec7f102bc1e2f4b7a2ab2
emmanue1 <java.decompiler@gmail.com>
2019-07-21 18:06:49 +0200

    Fix bug on local variable declarations

--- src/main/java/org/jd/core/v1/service/converter/classfiletojavasyntax/util/LocalVariableMaker.java
+++ src/main/java/org/jd/core/v1/service/converter/classfiletojavasyntax/util/LocalVariableMaker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2019 Emmanuel Dupuy.
+ * Copyright (c) 2008, 2019 Emmanuel Dupuy.
  * This project is distributed under the GPLv3 license.
  * This is a Copyleft license that gives the user the right to use,
  * copy and modify the code freely for non-commercial purposes.
@@ -182,9 +182,11 @@ public class LocalVariableMaker {
             AttributeLocalVariableTable localVariableTable = code.getAttribute("LocalVariableTable");
 
             if (localVariableTable != null) {
+                boolean staticFlag = (method.getAccessFlags() & FLAG_STATIC) != 0;
+
                 for (org.jd.core.v1.model.classfile.attribute.LocalVariable localVariable : localVariableTable.getLocalVariableTable()) {
                     int index = localVariable.getIndex();
-                    int startPc = (index==0) ? 0 : localVariable.getStartPc();
+                    int startPc = (!staticFlag && index==0) ? 0 : localVariable.getStartPc();
                     String descriptor = localVariable.getDescriptor();
                     String name = localVariable.getName();
                     AbstractLocalVariable lv;
