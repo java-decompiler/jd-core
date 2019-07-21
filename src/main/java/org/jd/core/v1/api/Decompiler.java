7d8a365cde313f5ab874b585143173cad00c488a
emmanue1 <java.decompiler@gmail.com>
2019-07-21 18:12:14 +0200

    Fix indentation

--- src/main/java/org/jd/core/v1/api/Decompiler.java
+++ src/main/java/org/jd/core/v1/api/Decompiler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2019 Emmanuel Dupuy.
+ * Copyright (c) 2008, 2019 Emmanuel Dupuy.
  * This project is distributed under the GPLv3 license.
  * This is a Copyleft license that gives the user the right to use,
  * copy and modify the code freely for non-commercial purposes.
@@ -13,7 +13,7 @@ import org.jd.core.v1.api.printer.Printer;
 import java.util.Map;
 
 public interface Decompiler {
-	void decompile(Loader loader, Printer printer, String internalName) throws Exception;
+    void decompile(Loader loader, Printer printer, String internalName) throws Exception;
 
-	void decompile(Loader loader, Printer printer, String internalName, Map<String, Object> configuration) throws Exception;
+    void decompile(Loader loader, Printer printer, String internalName, Map<String, Object> configuration) throws Exception;
 }
