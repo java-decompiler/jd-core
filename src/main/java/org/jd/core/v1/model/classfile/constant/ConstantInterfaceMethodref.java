1d30f814e0b81452ed856c675bac842ac8e2f36d
nbauma109 <nbauma109@github.com>
2022-02-10 22:02:31 +0100

    moved 219 files to jd-util + updated to jd-util 1.1.2

--- src/main/java/org/jd/core/v1/model/classfile/constant/ConstantInterfaceMethodref.java
+++
@@ -1,8 +0,0 @@
-package org.jd.core.v1.model.classfile.constant;
-
-public class ConstantInterfaceMethodref extends ConstantMethodref {
-
-    public ConstantInterfaceMethodref(int classIndex, int nameAndTypeIndex) {
-        super(classIndex, nameAndTypeIndex);
-    }
-}
